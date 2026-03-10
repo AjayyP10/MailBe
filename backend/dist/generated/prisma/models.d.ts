c9b61e6c6027bb55b91fd478c662b6dc3d2e88ab
Ajayy <ajayypannicker@gmail.com>
2026-03-10 16:15:35 +0100

    Delete repository contents

--- backend/dist/generated/prisma/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/User';
-export type * from './models/Email';
-export type * from './models/Attachment';
-export type * from './commonInputTypes';
-//# sourceMappingURL=models.d.ts.map
--- backend/dist/services/ai.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const generateReply: (previousEmails: string[], prompt: string) => Promise<any>;
-export declare const scanForPhishing: (emailBody: string) => Promise<any>;
-export declare const summarizeAttachment: (content: string) => Promise<any>;
-//# sourceMappingURL=ai.d.ts.map
